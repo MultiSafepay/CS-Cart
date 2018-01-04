77ef41632c9b09b5033cd85244a00a69028a7d1c
Jasper Nadi <jasper.nadi@multisafepay.com>
2018-01-04 14:17:18 +0100

    PLGCSCS-36. Set correct payment fee id, the same for the shipping method

--- app/payments/multisafepay.php
+++ app/payments/multisafepay.php
@@ -328,7 +328,7 @@ if (defined('PAYMENT_NOTIFICATION')) {
                     }
 
                     $c_item = new MspItem($shipper['shipping'], 'Verzending', 1, $shiping_price, 'KG', 0);
-                    $c_item->SetMerchantItemId($key);
+                    $c_item->SetMerchantItemId('msp-shipping');
                     //$c_item->SetTaxTableSelector('S_'.$key.'_0');
 
                     if ($shiptaxselector) {
@@ -352,7 +352,7 @@ if (defined('PAYMENT_NOTIFICATION')) {
         }
 
         $c_item = new MspItem($order_info['payment_method']['payment'], 'Betaal na ontvangst Fee', 1, $surcharge_price, 'KG', 0);
-        $c_item->SetMerchantItemId($order_info['payment_method']['payment_id']);
+        $c_item->SetMerchantItemId('payment-fee');
 
         $ptax = $order_info['payment_method']['tax_ids'];
         foreach ($ptax as $key => $value) {
