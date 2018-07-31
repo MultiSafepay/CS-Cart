4a71cc4c03132c3b505331197c00e76b2ead34b1
Ronald Schonewille <RSchonewille@office.multisafepay.com>
2018-07-31 12:53:57 +0200

    PLGCSCS-46: Support direct transactions for ING'HomePay / Alipay

--- app/payments/multisafepay.php
+++ app/payments/multisafepay.php
@@ -434,7 +434,7 @@ if (defined('PAYMENT_NOTIFICATION')) {
         $msp->extravars = $order_info['payment_info']['issuer'];
     }
 
-    if (in_array ($processor_data['processor_params']['gateway'], array ('IDEAL', 'KBC'))) {
+    if (in_array ($processor_data['processor_params']['gateway'], array ('IDEAL', 'KBC', 'INGHOME', 'ALIPAY'))) {
         $url = $msp->startDirectXMLTransaction();
     } else {
         $url = $msp->startCheckout();
